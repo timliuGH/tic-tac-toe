6069dccb5a071bb1562fed8610996151477be89d
Timothy Liu <timsgithub@gmail.com>
2019-08-18 12:10:39 -0700

    Add test cases

--- main.cpp
+++ main.cpp
@@ -1,15 +1,67 @@
 #include <iostream>
 #include "Board.hpp"
 
+void test(Board game)
+{
+    game.print();
+    if (game.gameState() == X_WON)
+        std::cout << "X won!\n" << std::endl;
+    if (game.gameState() == O_WON)
+        std::cout << "O won!\n" << std::endl;
+}
+
 int main()
 {
     Board game;
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-            std::cout << game.spaces[i][j] << " ";
-        std::cout << std::endl;
-    }
+    std::cout << "---Testing rows---\n" << std::endl;
+    game.makeMove(1, 1, 'x');
+    game.makeMove(1, 2, 'x');
+    game.makeMove(1, 3, 'x');
+    test(game);
+
+    game.reset();
+    game.makeMove(2, 1, 'x');
+    game.makeMove(2, 2, 'x');
+    game.makeMove(2, 3, 'x');
+    test(game);
+
+    game.reset();
+    game.makeMove(3, 1, 'x');
+    game.makeMove(3, 2, 'x');
+    game.makeMove(3, 3, 'x');
+    test(game);
+
+    std::cout << "---Testing columns---\n" << std::endl;
+    game.reset();
+    game.makeMove(1, 1, 'o');
+    game.makeMove(2, 1, 'o');
+    game.makeMove(3, 1, 'o');
+    test(game);
+
+    game.reset();
+    game.makeMove(1, 2, 'o');
+    game.makeMove(2, 2, 'o');
+    game.makeMove(3, 2, 'o');
+    test(game);
+
+    game.reset();
+    game.makeMove(1, 3, 'o');
+    game.makeMove(2, 3, 'o');
+    game.makeMove(3, 3, 'o');
+    test(game);
+
+    std::cout << "--Testing diagonals--" << std::endl;
+    game.reset();
+    game.makeMove(1, 1, 'x');
+    game.makeMove(2, 2, 'x');
+    game.makeMove(3, 3, 'x');
+    test(game);
+
+    game.reset();
+    game.makeMove(1, 3, 'x');
+    game.makeMove(2, 2, 'x');
+    game.makeMove(3, 1, 'x');
+    test(game);
 
     return 0;
 }
