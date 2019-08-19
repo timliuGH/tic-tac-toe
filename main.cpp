269084df4de0e05b418dea596589e13b547f9515
Timothy Liu <timsgithub@gmail.com>
2019-08-19 04:26:33 -0700

    Add testing info

--- main.cpp
+++ main.cpp
@@ -1,17 +1,29 @@
 #include <iostream>
 #include "Board.hpp"
 
-void test(Board game)
+State test(Board game)
 {
     game.print();
     if (game.gameState() == X_WON)
+    {
         std::cout << "X won!\n" << std::endl;
+        return X_WON;
+    }
     if (game.gameState() == O_WON)
+    {
         std::cout << "O won!\n" << std::endl;
+        return O_WON;
+    }
     if (game.gameState() == UNFINISHED)
+    {
         std::cout << "Game is unfinished\n" << std::endl;
+        return UNFINISHED;
+    }
     if (game.gameState() == DRAW)
+    {
         std::cout << "Cat's cradle!\n" << std::endl;
+        return DRAW;
+    }
 }
 
 int main()
@@ -21,51 +33,59 @@ int main()
     game.makeMove(1, 1, 'x');
     game.makeMove(1, 2, 'x');
     game.makeMove(1, 3, 'x');
-    test(game);
+    if (test(game) != X_WON)
+        return 1;
 
     game.reset();
     game.makeMove(2, 1, 'x');
     game.makeMove(2, 2, 'x');
     game.makeMove(2, 3, 'x');
-    test(game);
+    if (test(game) != X_WON)
+        return 1;
 
     game.reset();
     game.makeMove(3, 1, 'x');
     game.makeMove(3, 2, 'x');
     game.makeMove(3, 3, 'x');
-    test(game);
+    if (test(game) != X_WON)
+        return 1;
 
     std::cout << "---Testing columns---\n" << std::endl;
     game.reset();
     game.makeMove(1, 1, 'o');
     game.makeMove(2, 1, 'o');
     game.makeMove(3, 1, 'o');
-    test(game);
+    if (test(game) != O_WON)
+        return 1;
 
     game.reset();
     game.makeMove(1, 2, 'o');
     game.makeMove(2, 2, 'o');
     game.makeMove(3, 2, 'o');
-    test(game);
+    if (test(game) != O_WON)
+        return 1;
 
     game.reset();
     game.makeMove(1, 3, 'o');
     game.makeMove(2, 3, 'o');
     game.makeMove(3, 3, 'o');
-    test(game);
+    if (test(game) != O_WON)
+        return 1;
 
     std::cout << "--Testing diagonals--" << std::endl;
     game.reset();
     game.makeMove(1, 1, 'x');
     game.makeMove(2, 2, 'x');
     game.makeMove(3, 3, 'x');
-    test(game);
+    if (test(game) != X_WON)
+        return 1;
 
     game.reset();
     game.makeMove(1, 3, 'x');
     game.makeMove(2, 2, 'x');
     game.makeMove(3, 1, 'x');
-    test(game);
+    if (test(game) != X_WON)
+        return 1;
 
     std::cout << "--Testing DRAW--" << std::endl;
     game.reset();
@@ -78,7 +98,10 @@ int main()
     game.makeMove(3, 1, 'o');
     game.makeMove(3, 2, 'x');
     game.makeMove(3, 3, 'x');
-    test(game);
+    if (test(game) != DRAW)
+        return 1;
+
+    std::cout << "All tests passed." << std::endl;
 
     return 0;
 }
